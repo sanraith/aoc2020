0fb6929a4f8af754c41cbfaf974bdc47f6d08830
Soma Zsják <zsjaks@gmail.com>
2020-11-08 17:18:28 +0100

    Added sample web worker.

--- src/solutions/day01.ts
+++ src/solutions/day01.ts
@@ -3,10 +3,10 @@ import SolutionBase from '../core/solutionBase';
 export default class Day01 extends SolutionBase {
 
     protected part1(): string {
-        return '';
+        return 'Day 1 part 1 solution';
     }
 
     protected part2(): string {
-        return '';
+        return 'Day 1 part 2 solution';
     }
 }
---
+++ src/web/app/solution.worker.ts
@@ -0,0 +1,14 @@
+/// <reference lib="webworker" />
+
+import { SolutionResult } from 'src/core/solutionProgress';
+import Day01 from 'src/solutions/day01';
+
+addEventListener('message', ({ data }) => {
+    const day = new Day01();
+    day.init('');
+    day.part1Async().subscribe({
+        next(x) {
+            postMessage('Worker result: ' + (<SolutionResult>x).result);
+        }
+    });
+});
